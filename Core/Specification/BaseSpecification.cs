8e311ce848eb7d984a48536061b0314201a14b97
Frnatisek Oplt <opltfrantisek@gmail.com>
2020-05-30 21:06:07 +0200

    Initial commit

---
+++ Core/Specification/BaseSpecification.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Core.Specification
+{
+    public class BaseSpecification<T> : ISpecifications<T>
+    {
+        public BaseSpecification()
+        {
+        }
+
+        public BaseSpecification(Expression<Func<T,bool>> criteria)
+        {
+            Criteria = criteria;
+        }
+
+        public Expression<Func<T, bool>> Criteria  {get;}
+
+        public List<Expression<Func<T, object>>> Includes {get;} = new List<Expression<Func<T, object>>>();
+
+        public Expression<Func<T, object>> OrderBy {get; private set;}
+
+        public Expression<Func<T, object>> OrderByDescending {get; private set;}
+
+        public int Take {get; private set;}
+
+        public int Skip {get; private set;}
+
+        public bool IsPagingEnabled {get; private set;}
+
+        protected void AddInclude(Expression<Func<T,object>> includeExpressin){
+            Includes.Add(includeExpressin);
+        }
+        protected void AddOrderBy(Expression<Func<T,object>> OrderByExpression){
+            OrderBy = OrderByExpression;
+        }
+         protected void AddOrderByDescending(Expression<Func<T,object>> OrderByDescExpression){
+            OrderByDescending = OrderByDescExpression;
+        }
+        protected void ApplyPaging(int skip, int take){
+            Skip = skip;
+            Take = take;
+            IsPagingEnabled = true;
+        }
+    }
+}
