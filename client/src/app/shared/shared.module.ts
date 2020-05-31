e08d16c7bdb6aad99138b74589c325e3168a1c4c
Frnatisek Oplt <opltfrantisek@gmail.com>
2020-05-31 13:14:11 +0200

    Add Search

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationModule} from 'ngx-bootstrap/pagination';
+import { PaginHeaderComponent } from './components/pagin-header/pagin-header.component';
+import { PagerComponent } from './components/pager/pager.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PaginHeaderComponent, PagerComponent],
   imports: [
     CommonModule,
     PaginationModule.forRoot()
   ],
-  exports: [PaginationModule]
+  exports: [
+    PaginationModule,
+  PaginHeaderComponent,
+  PagerComponent
+  ]
 })
 export class SharedModule { }
--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { IProduct } from '../shared/models/product';
 import { ShopService } from './shop.service';
 import { IBrand } from '../shared/models/brands';
@@ -11,6 +11,7 @@ import { ShopParams } from '../shared/models/shopParams';
   styleUrls: ['./shop.component.scss']
 })
 export class ShopComponent implements OnInit {
+  @ViewChild('search', { static: true}) searchTerm: ElementRef;
   products: IProduct[];
   brands: IBrand[];
   types: IType[];
@@ -54,18 +55,33 @@ export class ShopComponent implements OnInit {
   }
 onBrandSelected(brandId: number){
   this.shopParams.brandID = brandId;
+  this.shopParams.pageNumber = 1;
   this.getProducts();
 }
 onTypesSelected(typeId: number){
   this.shopParams.typeId = typeId;
+  this.shopParams.pageNumber = 1;
   this.getProducts();
 }
 onSortSelected(sort: string){
   this.shopParams.sort = sort;
+  this.shopParams.pageNumber = 1;
   this.getProducts();
 }
 onPageChange(event: any){
-  this.shopParams.pageNumber = event.page;
+  if ( this.shopParams.pageNumber !== event){
+    this.shopParams.pageNumber = event;
+    this.getProducts();
+  }
+}
+onSearch(){
+  this.shopParams.search = this.searchTerm.nativeElement.value;
+  this.shopParams.pageNumber = 1;
+  this.getProducts();
+}
+onReset(){
+  this.searchTerm.nativeElement.value = '';
+  this.shopParams = new ShopParams();
   this.getProducts();
 }
 }
